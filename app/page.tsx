f8a3c246cbae63d50bfec053da1324abe166dabc
Elias Martin <elias@sportsvisio.com>
2024-10-17 14:02:30 -0400

    added better charting

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ChartComponent } from "@/components/ChartComponent"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-start min-h-[90vh] py-2 mt-8">
       <ChartComponent />
     </div>
   )
